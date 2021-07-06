76d69c938a4fe38eb1e99880d2f841087d4f69cf
Kaseem Bradley <kaseembradley@Dees-iphone.localdomain>
2021-07-06 19:31:35 -0400

    completed demo 06

--- src/demonstration_06.py
+++ src/demonstration_06.py
@@ -7,6 +7,16 @@ We will consider `a, e, i, o, u as vowels for this challenge (but not y).
 
 The input string will only consist of lower case letters and/or spaces.
 """
+
+
 def get_count(input_str):
     # Your code here
+    amount = 0
+    for char in input_str.lower():
+        if char in 'aeiou':
+            amount += 1
+    return amount
 
+
+s = "kaseem bradleyA"
+print(get_count(s))
